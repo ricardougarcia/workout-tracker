3707005e74c89d0d038faeb8047ed572abb2f12f
Ricardo Garcia <ricardougarcia@gmail.com>
2021-12-10 20:36:20 -0500

    deploying

--- server.js
+++ server.js
@@ -11,10 +11,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// });
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/ricos-database",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  }
+);
 
 // routes
 app.use(routes);
